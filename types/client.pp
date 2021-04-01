b941061b38bbdcf49d5ff716a24687cd682b788d
Steve Brown <steve.brown@thebrownnet.co.uk>
2021-04-01 20:14:35 +0100

    fix client type

--- types/client.pp
+++ types/client.pp
@@ -1,5 +1,18 @@
 # @summary
 #   A hash of client overrides
+# @example
+#   {
+#      name => 'My Laptop',
+#      tags => ['my_tag'],
+#      ids  => ['00:1B:44:11:3A:B7'],
+#      use_global_settings => false,
+#      filtering_enabled => true,
+#      parental_enabled => false,
+#      safesearch_enabled => false,
+#      use_global_blocked_services => false,
+#      blocked_services => ['Facebook'],
+#      upstreams => ['8.8.8.8']
+#   }
 type Adguard::Client = Struct[
     name => String,
     tags => Optional[Array],
@@ -9,6 +22,6 @@ type Adguard::Client = Struct[
     parental_enabled  => Optional[Boolean],
     safesearch_enabled => Optional[Boolean],
     use_global_blocked_services => Boolean,
-    blocked_services => Optional[Array],
+    blocked_services => Optional[Array[Adguard::Blocked_service]],
     upstreams => Optional[Array]
 ]
